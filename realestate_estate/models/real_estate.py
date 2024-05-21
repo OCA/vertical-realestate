bee128c002629a79295a7db3e1311875937b3724
Ronny Montano <rmontano1992@gmail.com>
2024-05-21 15:45:03 -0600

    [16.0][FIX] Update create method with ref value

--- realestate_estate/models/real_estate.py
+++ realestate_estate/models/real_estate.py
@@ -1,7 +1,7 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class RealEstate(models.Model):
@@ -46,3 +46,17 @@ class RealEstate(models.Model):
 
     def open_parent(self):
         return self.partner_id.open_parent()
+
+    @api.model
+    @api.returns("self", lambda value: value.id)
+    def create(self, vals):
+        vals = self._create_vals(vals)
+        self_context = self
+        if "type" in vals:
+            self_context = self.with_context(default_type=vals.get("type"))
+        return super(RealEstate, self_context).create(vals)
+
+    @api.model
+    def _create_vals(self, vals):
+        """Override this in child classes in order to add default values."""
+        return vals
