53426e48c27eefddd8f84a0583b5063b36e7f2a4
Ronny Montano <rmontano1992@gmail.com>
2024-05-21 13:37:54 -0600

    [16.0][MIG] Migrate realestate_estate

---
+++ realestate_estate/models/real_estate.py
@@ -0,0 +1,38 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class RealEstate(models.Model):
+    _name = "real.estate"
+    _inherits = {'res.partner': 'partner_id'}
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Real Estate"
+
+    partner_id = fields.Many2one(
+        string="Related Partner",
+        comodel_name="res.partner",
+        required=True,
+        auto_join=True,
+        ondelete="cascade",
+        index=True
+    )
+    type = fields.Selection(
+        default=lambda s: s._name, related="partner_id.type", readonly=False,
+    )
+    type_id = fields.Many2one(
+        comodel_name="real.estate.type", required=True, ondelete="restrict", index=True,
+    )
+    ref = fields.Char(
+        default=lambda self: self.env["ir.sequence"].next_by_code("real.estate"),
+        string="Reference",
+        index=True,
+        copy=False,
+        readonly=True,
+    )
+    short_description = fields.Char(translate=True, size=50, )
+    description = fields.Text(translate=True, )
+
+    def open_parent(self):
+        return self.partner_id.open_parent()
